1ff74e0b673012212e43502251d8b8a6a7a36fe1
Vinayaka V Nayak <106983430+vinayak-v-nayak@users.noreply.github.com>
2024-07-08 20:43:02 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Vinayaka V Nayak. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'nayakvinayaka22@gmail.com',
   phone: '+91 7349678879',
-  address: '8-211/1 Mahalakshmi Parijatha Compound near Anandi Maidan Nakre Road Karkala Taluk Udupi 576117',
+  address: 'Karkala Udupi KA(20)',
   github: 'https://github.com/vinayak-v-nayak',
   facebook: 'https://www.facebook.com/',
   linkedIn: 'https://www.linkedin.com/in/vinayaka-v-nayak',
